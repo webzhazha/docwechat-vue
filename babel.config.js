0359014d5f5f8df59044e592bf8e36709e12a463
liuhaitao <liuhaitao@91160.com>
2020-04-28 15:22:26 +0800

    fix:去除log

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
-const plugins = ["@vue/babel-plugin-transform-vue-jsx"]
-// 生产环境移除console
-if(process.env.NODE_ENV === 'production') {
-  plugins.push("transform-remove-console")
-}
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset",...plugins],
   plugins: [
@@ -10,15 +6,6 @@ module.exports = {
       libraryName: 'vant',
       libraryDirectory: 'es',
       style: true
-    }, 'vant'],
-    [
-      '@vue/app', {
-        modules: false,
-        targets: {
-          browsers: ["> 1%", "last 2 versions", "not ie <= 8", "Android >= 4", "iOS >= 8"]
-        },
-        useBuiltIns: 'entry',
-      }
-    ]
+    }, 'vant']
   ]
 };
